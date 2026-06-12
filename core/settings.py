db9fb9005e03fbe4993f45cc4343d05bfadca5a3
Mohhamadaminn <mohamad4aminm@gmail.com>
2026-06-12 14:51:53 +0330

    add I18N to project

--- core/settings.py
+++ core/settings.py
@@ -106,12 +106,18 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+LOCALE_PATHS = [
+    BASE_DIR / 'menu/locale',
+]
+
+
+LANGUAGE_CODE = 'fa'
+
+
+TIME_ZONE = 'Asia/Tehran'
 
 USE_I18N = True
-
 USE_TZ = True
 
 
@@ -122,9 +128,6 @@ STATIC_URL = 'static/'
 
 
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
