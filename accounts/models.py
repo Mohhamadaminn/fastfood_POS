a500f4e3b4af1cf9b55d9342771f9aba55067854
Mohhamadaminn <mohamad4aminm@gmail.com>
2026-06-21 16:58:33 +0330

    add is_owner field to our CustmUser

--- accounts/models.py
+++ accounts/models.py
@@ -2,5 +2,5 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
-class Customuser(AbstractUser):
-    pass
+class CustomUser(AbstractUser):
+    is_owner = models.BooleanField(default=False)
