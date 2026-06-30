89aa213f2fb2955c1fe53f81fb3b8542b67aa02e
Mohhamadaminn <mohamad4aminm@gmail.com>
2026-06-30 15:15:01 +0330

    just changed the unfold to unfold-admin

--- menu/admin.py
+++ menu/admin.py
@@ -1,3 +1,5 @@
+import jdatetime
+jdatetime.set_locale('fa_IR')
 from django.contrib import admin
 from unfold.admin import ModelAdmin
 from .models import (
@@ -5,26 +7,34 @@ from .models import (
     Order,
     OrderItem,
 )
- 
- 
+
+
 @admin.register(Product)
 class ProductAdmin(ModelAdmin):
     pass
- 
- 
+
+
 @admin.register(Order)
 class OrderAdmin(ModelAdmin):
     list_display = (
         'id',
-        'created_at',
+        'jalali_created_at',
         'status',
         'total_price',
     )
     list_filter = (
         'status',
     )
- 
- 
+
+    def jalali_created_at(self, obj):
+        if not obj.created_at:
+            return ''
+        jalali = jdatetime.datetime.fromgregorian(datetime=obj.created_at)
+        return jalali.strftime('%Y/%m/%d - %H:%M')
+
+    jalali_created_at.short_description = 'تاریخ ایجاد'
+
+
 @admin.register(OrderItem)
 class OrderItemAdmin(ModelAdmin):
     pass
